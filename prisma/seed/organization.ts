66edcf79e9803681a7c8203bed49779d0ca79209
lou <dustyfingers01257@gmail.com>
2022-09-17 22:30:35 -0600

    fixed org seeding

--- prisma/seed/organization.ts
+++ prisma/seed/organization.ts
@@ -3,19 +3,16 @@ import { PrismaClient } from '@prisma/client';
 
 const generateOrganizations = async (prisma: PrismaClient) => {
     const customerUsers = await prisma.customerUser.findMany();
-    console.log(customerUsers);
-
     for (let i = 0; i < customerUsers.length; i++) {
         const currentCustomer = customerUsers[i];
         await prisma.organization.create({
             data: {
                 created_at: new Date(),
                 updated_at: new Date(),
+                name: faker.company.name(),
                 customer: {
-                    connectOrCreate: {
-                        where: {
-                            id: currentCustomer.id,
-                        },
+                    connect: {
+                        id: currentCustomer.id,
                     },
                 },
             },
